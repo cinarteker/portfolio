d707c6f0afed526734d6b738106cefbab8d2f439
Davis Song <davissong01@gmail.com>
2023-10-07 23:51:21 -0700

    chore: App.js cleanup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/Navbar';
 import Box from '@mui/material/Box';
@@ -6,20 +5,10 @@ import { Outlet } from 'react-router-dom';
 
 function App() {
   return (
-    <Box
-        sx={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: '100vh',
-        width: '100vw',
-        backgroundColor: '#0D0D0D',
-        flexDirection: 'column',
-      }}>
+    <Box>
         <ResponsiveAppBar></ResponsiveAppBar>
         <Outlet />
     </Box>
-
   );
 }
 
