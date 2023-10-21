49400e73cd1732958a1abbc2c72e854796fc9123
Davis Song <davissong01@gmail.com>
2023-10-21 14:34:35 -0700

    feat: add window scroll

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import { Box } from "@mui/material";
+import { useEffect } from "react";
 import ResponsiveAppBar from "./components/Navbar";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
   return (
     <Box
       sx={{
